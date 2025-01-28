c2d46d3460f4af23a2f365b3bb16afb5cdd9c063
WoozyMasta <kagbe.leviy@gmail.com>
2025-01-29 02:47:40 +0300

    fix: alignment of structs

--- cli/config.go
+++ cli/config.go
@@ -17,13 +17,13 @@ It includes bot settings, a list of servers to monitor,
 logging configuration, and other relevant parameters.
 */
 type Config struct {
-	Bot struct {
+	Logging Logging        `yaml:"logging,omitempty"` // Logging configuration
+	Servers []ServerConfig `yaml:"servers"`           // List of server configurations
+	Bot     struct {
 		Token          string        `yaml:"token"`                         // Discord bot token
 		UpdateInterval time.Duration `yaml:"update_interval" default:"30s"` // Interval for status updates
 		Concurrency    int           `yaml:"concurrency" default:"10"`      // Number of concurrent operations
 	} `yaml:"bot"`
-	Servers []ServerConfig `yaml:"servers"`           // List of server configurations
-	Logging Logging        `yaml:"logging,omitempty"` // Logging configuration
 
 	prevCumulativeOnline int // Internal state to track previous cumulative online count
 }
@@ -34,23 +34,26 @@ ServerConfig represents the configuration for an individual server.
 It includes server connection details and Discord channel/category settings.
 */
 type ServerConfig struct {
-	// Fields to store the previous state hashes for channels and categories
-
-	prevChannelHash  uint32 // Previous hash for the channel
-	prevCategoryHash uint32 // Previous hash for the category
-
 	// Configuration data
 
 	ID           string `yaml:"id"`                            // Unique identifier for the server
 	Host         string `yaml:"host" default:"127.0.0.1"`      // Server host address
-	Port         int    `yaml:"port" default:"27016"`          // Server port
-	BufferSize   uint16 `yaml:"buffer_size" default:"1024"`    // Buffer size for A2S queries
-	Timeout      int    `yaml:"timeout" default:"3"`           // Timeout in seconds for A2S queries
 	ChannelID    string `yaml:"channel_id,omitempty"`          // Discord channel ID to update
 	ChannelName  string `yaml:"channel_name,omitempty"`        // Template for channel name
 	ChannelDesc  string `yaml:"channel_description,omitempty"` // Template for channel description
 	CategoryID   string `yaml:"category_id,omitempty"`         // Discord category ID to update
 	CategoryName string `yaml:"category_name,omitempty"`       // Template for category name
+	Port         int    `yaml:"port" default:"27016"`          // Server port
+	Timeout      int    `yaml:"timeout" default:"3"`           // Timeout in seconds for A2S queries
+
+	// Fields to store the previous state hashes for channels and categories
+
+	prevChannelHash  uint32 // Previous hash for the channel
+	prevCategoryHash uint32 // Previous hash for the category
+
+	// Configuration data again (aligned)
+
+	BufferSize uint16 `yaml:"buffer_size" default:"1024"` // Buffer size for A2S queries
 }
 
 /*
